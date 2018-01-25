b40f0cd8cdeb22d8f4349e9b29d4676f34ecbefd
Kadin <kadinvanvalin@yahoo.com>
2018-01-25 11:51:02 -0500

    add cd playing config with test

---
+++ src/main/java/com/kadinvanvalin/spring_in_action/spring_action/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,9 @@
+package com.kadinvanvalin.spring_in_action.spring_action.soundsystem;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
+
